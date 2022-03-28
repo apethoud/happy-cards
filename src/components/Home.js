628e6005eed3774f4a4d68a0e815ac79a1b555f1
Andrew Pethoud <andrewpethoud@gmail.com>
2022-03-27 20:13:01 -0700

    feat(home): add marketing copy

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+export default function Home() {
+  return (
+    <>
+      <div className="Home-HeroSection">
+        <h1>Send them a smile with HappyCards!</h1>
+        <h2>
+          Choose from thousands of uniquely-themed holiday cards for 500+
+          different holidays
+        </h2>
+      </div>
+      <div className="Home-HighlightsSection">
+        <div>
+          <p>Select a Holiday</p>
+          <p>Choose from 500+ holidays, the most of any e-card maker.</p>
+        </div>
+        <div>
+          <p>Pick a Design</p>
+          <p>
+            Explore hundreds of amazing designs curated by our expert designers.
+          </p>
+        </div>
+        <div>
+          <p>Let Us Deliver It</p>
+          <p>
+            We'll automatically send it to your loved one on a date you select.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
