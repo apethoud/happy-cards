da76133b7d744e742c0557b741edb5f49cf86a5d
Andrew Pethoud <andrewpethoud@gmail.com>
2022-03-29 13:48:15 -0700

    feat(home): changed header to use inline styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,9 @@ export default function Home() {
   return (
     <div className="Home-Container">
       <div className="Home-HeroSection">
-        <h1>Send them a smile with HappyCards!</h1>
+        <h1 style={{ padding: 6, backgroundColor: "darkturquoise" }}>
+          Send them a smile with HappyCards!
+        </h1>
         <h2>
           Choose from thousands of uniquely-themed holiday cards for 500+
           different holidays
