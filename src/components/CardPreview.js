67be21a8f61c31f00ed6872c582e84678584b68b
Andrew Pethoud <andrewpethoud@gmail.com>
2022-05-20 14:57:58 -0700

    style card preview text and placeholders

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -1,11 +1,40 @@
 export default function CardPreview(props) {
   const { recipientName, selectedHolidayName, senderName } = props;
+
   return (
     <>
-      <div>Card Preview</div>
-      <div>{recipientName}</div>
-      <div>{selectedHolidayName}</div>
-      <div>{senderName}</div>
+      <div style={{ display: "flex" }}>
+        <div className="CardPreview-Text">Hey&nbsp;</div>
+        {recipientName ? (
+          <div className="CardPreview-Text">{recipientName}</div>
+        ) : (
+          <div className="CardPreview-Text CardPreview-PlaceholderText">
+            Recipient Name
+          </div>
+        )}
+        <div className="CardPreview-Text">!</div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div className="CardPreview-Text">I hope you have a great&nbsp;</div>
+        {selectedHolidayName ? (
+          <div className="CardPreview-Text">{selectedHolidayName}</div>
+        ) : (
+          <div className="CardPreview-Text CardPreview-PlaceholderText">
+            Holiday
+          </div>
+        )}
+        <div className="CardPreview-Text">!</div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div className="CardPreview-Text">From&nbsp;</div>
+        {senderName ? (
+          <div className="CardPreview-Text">{senderName}</div>
+        ) : (
+          <div className="CardPreview-Text CardPreview-PlaceholderText">
+            Sender Name
+          </div>
+        )}
+      </div>
     </>
   );
 }
